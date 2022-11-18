7179c34751ad149788893b19b0727dfcefd0e4ff
camerobarrosofrancisco@gmail.com <camerobarrosofrancisco@gmail.com>
2022-11-18 13:34:44 +0100

    Boletin2

---
+++ Boletin2.py
@@ -0,0 +1,36 @@
+"""Escribir una expresión lógica que cumpla:"""
+#a)
+
+#b)
+dia='M'
+print(dia=='M')
+#c)
+num1=6
+print(num1%3 == 0)
+#d)
+nota=10
+print(nota>=5 and nota<=10)
+#e)
+nota1=5
+nota2=4
+nota3=10
+print(nota1>=5 and nota1<=10 and nota2>=5 and nota2<=10 and nota3>=5 and nota3<=10)
+
+"""Escribir una expresión lógica que cumpla:"""
+#a)
+nota1=5
+nota2=2
+nota3=10
+print(nota1>=5 and nota1<=10 and nota2>=5 and nota2<=10 and nota3>=5 and nota3<=10)
+#b)
+saldo=100
+descubierto=6
+print(not(saldo<1000 or descubierto<5 and descubierto>=0))
+#c)*no funciona*
+asignaturasAprobadas=7
+asignaturasCurso=8
+print(asignaturasAprobadas*30//100)
+#d)
+mes=12
+dia=30
+print(mes>=1 and mes<=12 and dia>=1 and dia<=30)
